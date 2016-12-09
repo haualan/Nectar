44bb2f8158dfbd8f645f9bc113bd7daed50de6db
Alan Hau <wch82@cornell.edu>
2016-12-09 18:22:48 +0800

    Email auto pop

--- profiles/models.py
+++ profiles/models.py
@@ -76,9 +76,9 @@ class User(AbstractEmailUser):
 
 
   def save(self, *args, **kwargs):
-    if self.email == "":
-      self.email = 'alan+temp{}@firstcodeacademy.com'.format(self.id)
     super(User, self).save(*args, **kwargs) # Call the "real" save() method.
+    self.email = 'alan+temp{}@firstcodeacademy.com'.format(self.id)
+    self.save()
 
 
 
