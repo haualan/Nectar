4dc55b031e3973fefbcd8222e322a34f375133a6
Alan Hau <wch82@cornell.edu>
2017-01-10 11:56:59 +0800

    Add uid to feed

--- feed/apiviews.py
+++ feed/apiviews.py
@@ -62,7 +62,8 @@ class FeedView(mixins.ListModelMixin, viewsets.GenericViewSet):
             'type' : 'TrophyRecord',
             'id' : i.id,
             'user_avatar': i.user_avatar,
-            'user_name': "{} {}".format(i.user_fname, i.user_lname),
+            'user_name': i.user.displayName,
+            'user_id': i.user.id,
             'trophy_avatar': i.trophy_avatar,
             'trophy_name': i.trophy_name
 
@@ -87,6 +88,7 @@ class FeedView(mixins.ListModelMixin, viewsets.GenericViewSet):
             'id' : i.id,
             'user_avatar': i.user_avatar,
             'user_name':i.user.displayName,
+            'user_id': i.user.id,
             # 'user_name': "{} {}".format(i.user_fname, i.user_lname)
 
         } for i in cr]
@@ -111,6 +113,7 @@ class FeedView(mixins.ListModelMixin, viewsets.GenericViewSet):
             'name': i.name,
             'user_avatar': i.user_avatar,
             'user_name': i.user.displayName,
+            'user_id': i.user.id,
 
         } for i in p]
 
