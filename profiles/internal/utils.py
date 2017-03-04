75c686eef2534830c3f7a23011386f229e720875
Alan Hau <wch82@cornell.edu>
2017-03-04 17:00:48 +0800

    Emails trigger internal

--- profiles/internal/utils.py
+++ profiles/internal/utils.py
@@ -3,10 +3,81 @@ from django.utils import timezone
 from course.models import internalEmailExclusionRegex
 from django.db.models import FloatField, IntegerField
 from django.db.models.functions import Cast
+import StringIO, requests
+from django.conf import settings
+
+
+internalEmailRecipients = [
+  # 'michelle@firstcodeacademy.com', 
+  'alan@firstcodeacademy.com',
+  ]
+
+def send_internal_email(subject, text, html, file, subdomain = 'hk' ):
+  """
+  base function for sending email
+  """
+
+  # hk serves as a fallback
+  if len(subdomain) == 0:
+    subdomain = 'hk'
+
+  r = requests.post(
+    settings.MAILGUN_API_URL,
+    auth=("api", settings.MAILGUN_KEY),
+    data={"from": settings.SUBDOMAINSPECIFICMAPPING.get(subdomain).get('emailFrom'),
+          "h:Reply-To": settings.SUBDOMAINSPECIFICMAPPING.get(subdomain).get('emailFrom'),
+          "to": ', '.join(internalEmailRecipients),
+          "subject": subject,
+          "text": text,
+          "html": html,
+          "files"=[("attachment", file)],
+          ""
+
+          })
+
+  return r.status_code
+
+def guardiansPendingPurchaseEmail(request):
+  """
+  wrapper for guardiansPendingPurchase, sends email 
+
+  """
+  r = guardiansPendingPurchase
+
+  csvfile = StringIO.StringIO()
+  csvwriter = csv.writer(csvfile)
+
+  # define the columns, also serve as keys
+  cols = [ 'hoursWithoutPurchase','guardianEmail', 'guardianPhone', 'guardianAddress' ]
+
+  # write header
+  csvwriter.writerow(cols)
+
+  for row in r:
+    csvwriter.writerow((row.get(i) for i in cols))
+
+
+  file = csvfile.getvalue()
+
+  text = """
+    This report contains all users who signed up as a guardian after landing on the payments page (thereby showing high interest), 
+    but haven't made purchase after 24 hours.
+    - If the same user has not made a purchase for up till 7 days, the user will be dropped from the report (user no longer interested)
+    - users signed up for less than 24 hrs are excluded from the report (maybe user is still deciding)
+    """
+
+  send_internal_email(
+    subject = 'test guardiansPendingPurchase report',
+    text = text,
+    html = html,
+    file  = file
+  )
 
 
 
-def guardiansPendingPurchase(request):
+  pass
+
+def guardiansPendingPurchase():
   """
   finds all users (that are guardians) who has registered but hasn't made a purchase in the system
   - the idea behind this is that a buyer landed on the page, looking to pay but might be deterred from making actual payment what gives...
@@ -43,6 +114,9 @@ def guardiansPendingPurchase(request):
     }
     for u in r if (((now - u.date_joined).total_seconds() / 3600.0) >= detectionWindowHours)
   ]
+
+  # sort by hoursWithoutPurchase
+  results.sort(key= lambda x: x['hoursWithoutPurchase'] )
 
   return results
 
